eb899dd45bedd34c7efbf1027b5c1c6e9b37bfe3
hjyouns <hjyouns@gmail.com>
2026-04-28 13:31:14 +0900

    app.js test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [file, setFile] = useState(null);
   const [uploadedFileName, setUploadedFileName] = useState(null);
 
-  const albAddress = "http://lshalb-2124324195.ap-northeast-2.elb.amazonaws.com";
+  const albAddress = process.env.REACT_APP_API_BASE_URL;
 
   const callBackend = async () => {
     setMessage('호출 중...');
